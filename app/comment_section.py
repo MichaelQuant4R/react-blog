9be96cb885e657e1acbb6de1e03e380d9a1e56d5
MichaelQuant4R <Michael.R2014@yahoo.co.uk>
2021-08-02 14:03:57 +0100

    cross_origin comment_section.py

--- app/comment_section.py
+++ app/comment_section.py
@@ -2,6 +2,7 @@ from flask import (Blueprint, request, jsonify)
 from .models import (db, User, Comment, Blog, Notify)
 from flask_jwt_extended import (create_access_token, current_user, jwt_required)
 import arrow
+from flask_cors import cross_origin
 
 app_com = Blueprint("comment_section", __name__)
 
