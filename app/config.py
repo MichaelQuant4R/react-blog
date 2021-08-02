9c46dadf14ea4b8675bfa8591355e8051893f889
MichaelQuant4R <Michael.R2014@yahoo.co.uk>
2021-08-02 12:11:00 +0100

    config.py os.environ.get

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 import os
 
-DATABASE_URL = os.environ("DATABASE_URL")
-JWT_SECRET_KEY = os.environ("JWT_SECRET_KEY")
+DATABASE_URL = os.environ.get("DATABASE_URL")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
